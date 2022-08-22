31f81c2b14551bc2486229ac2b4de3cb653943be
Himanshu Borole <himanshu.borole@gmail.com>
2022-08-22 09:55:03 +0530

    Changes to new route

--- src/routes/new.ts
+++ src/routes/new.ts
@@ -1,9 +1,20 @@
+import { requireAuth, validateRequest } from '@hbofficial/common';
 import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/orders', async (req: Request, res: Response) => {
-  res.send({});
-});
+router.post(
+  '/api/orders',
+  requireAuth,
+  [
+    body('ticketId').not().isEmpty().withMessage('Ticket id is required'),
+    body('ticketId').isMongoId().withMessage('Ticket id is invalid'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    res.send({});
+  }
+);
 
 export { router as createOrderRouter };
