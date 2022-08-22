72f39002331ec42659d57a11483fba97359dc13e
Himanshu Borole <himanshu.borole@gmail.com>
2022-08-22 10:46:59 +0530

    New route implementation

--- src/routes/new.ts
+++ src/routes/new.ts
@@ -1,8 +1,17 @@
-import { requireAuth, validateRequest } from '@hbofficial/common';
+import {
+  BadRequestError,
+  NotFoundError,
+  requireAuth,
+  validateRequest,
+} from '@hbofficial/common';
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
+import { Order, OrderStatus } from '../models/order';
+import { Ticket } from '../models/ticket';
 
 const router = express.Router();
+
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
 
 router.post(
   '/api/orders',
@@ -13,7 +22,36 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
-    res.send({});
+    const { ticketId } = req.body;
+
+    // Find the ticket the user is trying to order in the database
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    // Make sure that this ticket is not already reserved
+    const isReserved = await ticket.isReserved();
+    if (isReserved) {
+      throw new BadRequestError('Ticket is already reserved');
+    }
+
+    // Calculate an expiration date
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+
+    // Build the order and save it to the database
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      expiresAt: expiration,
+      status: OrderStatus.Created,
+      ticket,
+    });
+    await order.save();
+
+    // Publish an event saying that an order was created
+
+    res.status(201).send(order);
   }
 );
 
