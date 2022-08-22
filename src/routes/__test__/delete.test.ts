beaacb5a37470def493455e069cc85ba3ffa788b
Himanshu Borole <himanshu.borole@gmail.com>
2022-08-22 14:20:15 +0530

    Test publish events

--- src/routes/__test__/delete.test.ts
+++ src/routes/__test__/delete.test.ts
@@ -2,6 +2,7 @@ import request from 'supertest';
 import { app } from '../../app';
 import { Order, OrderStatus } from '../../models/order';
 import { Ticket } from '../../models/ticket';
+import { natsWrapper } from '../../nats-wrapper';
 
 it('marks an order as cancelled', async () => {
   // Create a ticket
@@ -29,4 +30,27 @@ it('marks an order as cancelled', async () => {
   // // fetch the order
   const udpatedOrder = await Order.findById(order.id);
   expect(udpatedOrder!.status).toEqual(OrderStatus.Cancelled);
+});
+
+it('emits an order cancelled event', async () => {
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20,
+  });
+  await ticket.save();
+
+  const user = global.signin();
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set('Cookie', user)
+    .send()
+    .expect(200);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
