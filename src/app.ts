0e8e6cedc92877fa27c54cb2d554f49f74b6fcd7
Himanshu Borole <himanshu.borole@gmail.com>
2022-08-22 08:55:28 +0530

    Init commit

---
+++ src/app.ts
@@ -0,0 +1,35 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import { NotFoundError, errorHandler, currentUser } from '@hbofficial/common';
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexRouter } from './routes';
+import { updateTicketRouter } from './routes/update';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+
+app.use(currentUser);
+
+app.use(indexRouter);
+app.use(showTicketRouter);
+app.use(createTicketRouter);
+app.use(updateTicketRouter);
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
