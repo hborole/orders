ca8b2467aab809f4e85eeb25cc8ccc0e762686e0
Himanshu Borole <himanshu.borole@gmail.com>
2022-08-23 09:17:29 +0530

    Blueprint of listeners

---
+++ src/events/listeners/ticket-created-listener.ts
@@ -0,0 +1,10 @@
+import { Listener, Subjects, TicketCreatedEvent } from '@hbofficial/common';
+import { Message } from 'node-nats-streaming';
+import { queueGroupName } from './queue-group-name';
+
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  subject: Subjects.TicketCreated = Subjects.TicketCreated;
+  queueGroupName = queueGroupName;
+
+  onMessage(data: TicketCreatedEvent['data'], msg: Message) {}
+}
