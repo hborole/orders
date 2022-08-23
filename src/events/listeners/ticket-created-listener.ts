4fc830149357e4b4dcd11e8150035d9b2f2c205f
Himanshu Borole <himanshu.borole@gmail.com>
2022-08-23 09:24:31 +0530

    Ticket created listener

--- src/events/listeners/ticket-created-listener.ts
+++ src/events/listeners/ticket-created-listener.ts
@@ -1,10 +1,17 @@
 import { Listener, Subjects, TicketCreatedEvent } from '@hbofficial/common';
 import { Message } from 'node-nats-streaming';
+import { Ticket } from '../../models/ticket';
 import { queueGroupName } from './queue-group-name';
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
   subject: Subjects.TicketCreated = Subjects.TicketCreated;
   queueGroupName = queueGroupName;
 
-  onMessage(data: TicketCreatedEvent['data'], msg: Message) {}
+  async onMessage(data: TicketCreatedEvent['data'], msg: Message) {
+    const { id, title, price } = data;
+
+    const ticket = Ticket.build({ id, title, price });
+    await ticket.save();
+    msg.ack();
+  }
 }
